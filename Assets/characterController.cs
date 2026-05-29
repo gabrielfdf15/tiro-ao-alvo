a48ed8a3fab6b7b6456ed255ad030b818754d57c
Gabriel <senhorruim97@gmail.com>
2026-05-29 15:43:41 -0300

    ooo

---
+++ Assets/characterController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float horizontalInput = Input.GetAxis("Horizontal");
+    rb2d.velocity += new Vector2(1,0) * horizontalInput;
+    }
+}
